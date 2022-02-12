45b3ea6d0bbcbc252a9e88e2fd71105cef33a36c
Mohamed Awnallah <mohamedmohey2352@gmail.com>
2022-02-12 10:43:19 +0200

    Display Quote to User

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-import json
+import json, glob, random
 from datetime import datetime
+from pathlib import Path
 Builder.load_file('design.kv')
 
 class LoginScreen(Screen):
@@ -11,7 +12,7 @@ class LoginScreen(Screen):
     def login(self, username, password):
         with open("users.json",'r') as file:
             users = json.load(file)
-            if username in users and users[username]['password'] == password:
+            if username.strip() in users and users[username]['password'] == password.strip():
                 self.manager.current = "login_screen_success"   
                 self.ids.login_wrong.text = ""
             else:
@@ -21,12 +22,11 @@ class SignUpScreen(Screen):
     def add_user(self, username, password):
         print(username,password)
         if  username and  password:
-            print("not empty")
             with open("users.json",'r') as file:
                 users = json.load(file)
 
-            users[username] = {"username":username,
-            "password":password,
+            users[username] = {"username":username.strip(),
+            "password":password.strip(),
             "created":datetime.now().strftime('%Y-%m-%d %H-%M-%S')
             }
             with open('users.json','w') as file:
@@ -47,7 +47,20 @@ class LoginScreenSuccess(Screen):
         self.manager.transition.direction = 'right'
         self.manager.current = 'login_screen'
 
+    def get_quote(self, feeling):
+        feeling = feeling.lower()
+        avaliable_feelings = glob.glob("quotes/*txt")
 
+        avaliable_feelings = [Path(filename).stem for
+         filename in avaliable_feelings]
+
+        if feeling in avaliable_feelings:
+            with open(f"quotes/{feeling}.txt") as file:
+                quotes = file.readlines()
+            self.ids.quote.text = random.choice(quotes)
+        else:
+            self.ids.quote.text = "Try another Feeling"
+       
 class RootWidget(ScreenManager):
     pass
 
